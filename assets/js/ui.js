41b07cf3c3832447411f66918eee89aff56be7ba
jeonghyejin <hyegineer@gmail.com>
2023-12-12 06:08:55 +0900

    commit

---
+++ assets/js/ui.js
@@ -0,0 +1,18 @@
+/*
+=============================================
+모달창 열고 닫기
+=============================================
+*/
+
+function openModal(id) {
+  $('#' + id).addClass('active');
+  $('#' + id).children('.custom-modal').scrollTop(0);
+}
+
+function closeModal(id) {
+  $('#' + id).removeClass('active');
+}
+
+function allCloseModal() {
+  $('.modal-wrapper').removeClass('active');
+}
